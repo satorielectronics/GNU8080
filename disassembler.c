7b524505ca69efdd6996e643d5410a69662e84af
Timothy <moequalitycontrol@gmail.com>
2022-10-08 13:19:42 -0500

    Update disassembler.c

--- disassembler.c
+++ disassembler.c
@@ -108,6 +108,38 @@ unsigned char *code = &codebuffer[pc]; //Stores a single byte from the ROM or al
     case 0x5d: printf("MOV    E,L"); break;
     case 0x5e: printf("MOV    E,M"); break;
     case 0x5f: printf("MOV    E,A"); break;
+    case 0x60: printf("MOV    H,B"); break;
+    case 0x61: printf("MOV    H,C"); break;
+    case 0x62: printf("MOV    H,D"); break;
+    case 0x63: printf("MOV    H,E"); break;
+    case 0x64: printf("MOV    H,H"); break;
+    case 0x65: printf("MOV    H,L"); break;
+    case 0x66: printf("MOV    H,M"); break;
+    case 0x67: printf("MOV    H,A"); break;
+    case 0x68: printf("MOV    L,B"); break;
+    case 0x69: printf("MOV    L,C"); break;
+    case 0x6a: printf("MOV    L,D"); break;
+    case 0x6b: printf("MOV    L,E"); break;
+    case 0x6c: printf("MOV    L,H"); break;
+    case 0x6d: printf("MOV    L,L"); break;
+    case 0x6e: printf("MOV    L,M"); break;
+    case 0x6f: printf("MOV    L,A"); break;
+    case 0x70: printf("MOV    M,B"); break;
+    case 0x71: printf("MOV    M,C"); break;
+    case 0x72: printf("MOV    M,D"); break;
+    case 0x73: printf("MOV    M,E"); break;
+    case 0x74: printf("MOV    M,H"); break;
+    case 0x75: printf("MOV    M,L"); break;
+    case 0x76: printf("HLT"); break; //SPECIAL
+    case 0x77: printf("MOV    M,A"); break;
+    case 0x78: printf("MOV    A,B"); break;
+    case 0x79: printf("MOV    A,C"); break;
+    case 0x7a: printf("MOV    A,D"); break;
+    case 0x7b: printf("MOV    A,E"); break;
+    case 0x7c: printf("MOV    A,H"); break;
+    case 0x7d: printf("MOV    A,L"); break;
+    case 0x7e: printf("MOV    A,M"); break;
+    case 0x7f: printf("MOV    A,A"); break;
 
 
 
